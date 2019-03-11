211bd2c019d08f0819679e4d09ac0eae6aba5ec7
flyaway <jolvmail@gmail.com>
2019-03-11 09:09:39 +0800

    add: tools

---
+++ command_tools/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo 'Installing tig'
+brew install tig
+echo 'Installing succeed'
+
+echo 'Installing fpp'
+brew install fpp
+echo 'Installing succeed'
+
+echo 'Installing gron'
+brew install gron
+echo 'Installing succeed'
+
+echo 'Installing bat'
+brew install bat
+echo 'Installing succeed'
+
+echo 'Installing fd'
+brew install fd
+echo 'Installing succeed'
+
+echo 'Installing fzf'
+brew install fzf
+echo 'Installing succeed'
